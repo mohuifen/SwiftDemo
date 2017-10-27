3dd533811562bd5faff306dcd68df41ec2575aae
Luckyfy <mohuifen@126.com>
2017-10-28 00:22:42 +0800

    完成todolist项目

--- 第二周/LRFTodoList/LRFTodoList/LRFNewTodoViewController.swift
+++ 第二周/LRFTodoList/LRFTodoList/LRFNewTodoViewController.swift
@@ -11,20 +11,89 @@ import UIKit
 class LRFNewTodoViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
     var model:LRFTodoModel?
+    var selectedType:Int?
+    
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var datePickerView: UIDatePicker!
+    
+    let grayImageArray = [UIImage(named:"friend50_gray"),UIImage(named:"phone50_gray"),UIImage(named:"cart50_gray"),UIImage(named:"travel50_gray")]
+
+    let imageArray = [UIImage(named:"friend50"),UIImage(named:"phone50"),UIImage(named:"cart50"),UIImage(named:"travel50")]
+    
+    // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let model = model {
+            selectedType = model.type
+            titleTextField.text = model.title
+            datePickerView.date = model.time
+            
+            self.title = "Edit Todo"
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - Action
     @IBAction func onDone(_ sender: Any) {
+        if let type = selectedType,
+            let title = titleTextField.text {
+            
+            if title.characters.count == 0 {
+                return
+            }
+            
+            var newModel = LRFTodoModel.init(type: type, title: title, time: datePickerView.date)
+            
+            if model != nil {
+                model?.type = type
+                model?.time = datePickerView.date
+                model?.title = title
+                
+                newModel = model!
+            }
+            
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "newOrAdd"), object: nil, userInfo: ["model":newModel])
+            navigationController?.popViewController(animated: true)
+        }
     }
+    // MARK: - collectionView delegate and dataSource
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell1", for: indexPath)
+
+        let imageView = cell.contentView.viewWithTag(100) as? UIImageView
+
+        if selectedType == indexPath.row {
+            imageView?.image = imageArray[indexPath.row]
+        } else {
+            imageView?.image = grayImageArray[indexPath.row]
+        }
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedType = indexPath.row
+        collectionView.reloadData()
+    }
+    // MARK: - 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     
 }
--- 第二周/LRFTodoList/LRFTodoList/LRFTodoListTableViewController.swift
+++ 第二周/LRFTodoList/LRFTodoList/LRFTodoListTableViewController.swift
@@ -12,97 +12,153 @@ class LRFTodoModel: NSObject {
     var type:Int
     var title:String
     var time:Date
+    var index:Int
     
-    init(type:Int, title:String, time:Date) {
+    convenience init(type:Int, title:String, time:Date) {
+       self.init(type: type, title: title, time: time, index: -1)
+    }
+    init(type:Int, title:String, time:Date, index: Int) {
         self.type = type
         self.title = title
         self.time = time
+        self.index = index
     }
 }
 
 class LRFTodoListTableViewController: UITableViewController {
 
-    var dataArray:[LRFTodoModel]?
+    var dataArray:[LRFTodoModel]? = []
+    
+    let imageArray = [UIImage(named:"friend60"),UIImage(named:"phone60"),UIImage(named:"cart60"),UIImage(named:"travel60")]
+    
+    // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        tableView.tableFooterView = UIView.init()
+        for index in 1...5 {
+            let model = LRFTodoModel.init(type: index%4, title: "\(index)", time: Date.init(timeIntervalSinceNow: 10000))
+            dataArray?.insert(model, at: 0)
+        }
+        NotificationCenter.default.addObserver(self, selector: #selector(onAddOrEditModel(notification:)), name: NSNotification.Name(rawValue: "newOrAdd"), object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
-    
+    // MARK: - Notifation
+    @objc func onAddOrEditModel(notification: NSNotification) {
+        if let model = notification.userInfo!["model"] as? LRFTodoModel {
+            if model.index >= 0 {// edit
+                dataArray![model.index] = model
+            } else { //add
+                dataArray?.insert(model, at: 0)
+            }
+            tableView.reloadData()
+        }
+    }
+    // MARK: - Action
     @IBAction func onEdit(_ sender: Any) {
+        self.tableView.setEditing(!tableView.isEditing, animated: true)
+        if let edit = sender as? UIBarButtonItem {
+            if tableView.isEditing {
+                
+                edit.title = "Done"
+            } else {
+                edit.title = "Edit"
+            }
+        }
         
+    }
+  
+    // MARK: - Logic
+    func imageForType(type: Int) -> UIImage {
+       
+        if let image = imageArray[type] {
+            return image
+        }
+        return UIImage()
     }
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataArray!.count
     }
 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        if let model = dataArray?[indexPath.row] {
+            
+            model.index = indexPath.row
+            
+            cell.imageView?.image = self.imageForType(type: model.type)
+            cell.textLabel?.text = model.title
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "YYYY-MM-dd"
+          
+            cell.detailTextLabel?.text = dateFormatter.string(from: model.time)
+
+        }
+        
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 16)
 
 
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // 设置传递的参数
-        self.performSegue(withIdentifier: "edit", sender: nil)
+        
+        if let model = dataArray?[indexPath.row]{
+            model.index = indexPath.row
+            // 设置传递的参数
+            self.performSegue(withIdentifier: "edit", sender: model)
+        }
         
     }
     
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            dataArray?.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.reloadData()
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
+    
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        if let model = dataArray?.remove(at: fromIndexPath.row) {
+            dataArray?.insert(model, at: to.row)
+            tableView.reloadData()
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
+    
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
-
+    
     
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "edit" {
-            if let destination:LRFNewTodoViewController = segue.destination {
-                
+            if let destination = segue.destination as? LRFNewTodoViewController,
+                let model = sender as? LRFTodoModel{
+                destination.model = model
             }
             
         }
