7ca90ce2f6a7fc9c314b37da780894b61d6f0acb
lvruifei <lvruifei@huobi.com>
2017-10-29 15:03:51 +0800

    完成 Swipeablecell 项目

--- 第二周/LRFSwipeableCell/LRFSwipeableCell/LRFTableViewController.swift
+++ 第二周/LRFSwipeableCell/LRFSwipeableCell/LRFTableViewController.swift
@@ -10,34 +10,48 @@ import UIKit
 
 class LRFTableViewController: UITableViewController {
 
-    var dataArray: [[String:String]] = []
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var dataArray: [[String:String]]? = {
         do {
-            if let path = Bundle.main.path(forResource: "Contacts", ofType: "plsit") {
+            let path = Bundle.main.path(forResource: "Contacts", ofType: "plist")
+            if let path = Bundle.main.path(forResource: "Contacts", ofType: "plist") {
                 let url = URL.init(fileURLWithPath: path)
                 let data = try Data(contentsOf: url)
-                let array = try PropertyListDecoder().decode([String].self, from: data)
-                dataArray.append((array as? [String:String])!)
+                let array = try PropertyListDecoder().decode([[String:String]].self, from: data)
+                return array
             }
         } catch let error {
             print(error)
         }
+        return []
 
+    }()
+    
+    lazy var shareAlert = UIActivityViewController(activityItems:[
+        UIActivityType.message,
+        UIActivityType.mail,
+        
+        ], applicationActivities: [
+
+        ])
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let count = dataArray.count {
+        if let count = dataArray?.count {
             return count
         }
         return 0
@@ -46,34 +60,47 @@ class LRFTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        if let image = dataArray[indexPath.row]["headPortraitStr"]
-            let name = dataArray[indexPath.row]["name"] {
-            cell.imageView?.image = image
-            cell.textLabel?.text = name
+        if let dic = dataArray?[indexPath.row],
+            let imageView = cell.contentView.viewWithTag(100) as? UIImageView,
+            let label = cell.contentView.viewWithTag(200) as? UILabel {
+            
+            imageView.image = UIImage(named:dic["headPortraitStr"] ?? "")
+            label.text = dic["name"]
 
         }
+        cell.backgroundColor = UIColor(red: 252/255, green: 255/255, blue: 237/255, alpha: 1)
         return cell
     }
-    
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = UIContextualAction(style: UIContextualAction.Style.normal, title: "🗑️\nDelete") { (action, view, finished) in
+            self.dataArray?.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+            finished(true)
+        }
+        delete.backgroundColor = UIColor(red: 123/255, green: 119/255, blue: 124/255, alpha: 1)
+        let share = UIContextualAction(style: UIContextualAction.Style.normal, title: "🤗\nShare") { (action, view, finished) in
+            self.present(self.shareAlert, animated: true, completion: {
+                print("Share OK");
+            })
+            finished(true)
+
+        }
+        share.backgroundColor = UIColor(red: 252/255, green: 26/255, blue: 32/255, alpha: 1)
+
+        let download = UIContextualAction(style: UIContextualAction.Style.normal, title: "⬇️\n Download ") { (action, view, finished) in
+            print("Download OK");
+            finished(true)
+        }
+        download.backgroundColor = UIColor(red: 8/255, green: 1/255, blue: 249/255, alpha: 1)
+
+        return UISwipeActionsConfiguration(actions: [download,share,delete])
     }
-    */
+ 
 
 }
