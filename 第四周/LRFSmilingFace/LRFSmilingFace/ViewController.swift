fe9d264bddd7c40bac5c89de20eee8af70ca5b4d
lvruifei <lvruifei@huobi.com>
2017-11-11 19:39:57 +0800

    添加 SmilingFace 项目

---
+++ 第四周/LRFSmilingFace/LRFSmilingFace/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  LRFSmilingFace
+//
+//  Created by 吕瑞斐 on 2017/11/11.
+//  Copyright © 2017年 Laura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var left: UIImageView!
+    
+    @IBOutlet weak var right: UIImageView!
+    
+    @IBOutlet weak var center: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.titleLabel.isHidden = true
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.8, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.2, options: UIViewAnimationOptions.init(rawValue: 1), animations: {
+            
+            self.left.center = CGPoint(x: self.view.frame.size.width - self.left.center.x, y: self.left.center.y+30)
+            
+            self.right.center = CGPoint(x: self.view.frame.size.width - self.right.center.x, y: self.right.center.y+30)
+            
+            
+            let frame:CGRect = CGRect(x: self.center.frame.origin.x, y: self.center.frame.origin.y, width: self.center.frame.size.width, height: 180)
+            
+            self.center.frame = frame
+            
+            self.view.layoutIfNeeded()
+        }) { (finished) in
+            UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.8, options: UIViewAnimationOptions.init(rawValue: 0), animations: {
+                self.titleLabel.isHidden  = false
+            }, completion: nil)
+        }
+       
+    }
+
+
+}
+
